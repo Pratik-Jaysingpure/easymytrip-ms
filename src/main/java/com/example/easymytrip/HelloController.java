df1fe012bc7c67eb69e67603ffd39c22c31476e9
pratik-jaysingpure <pratikjaysingpure9@gmail.com>
2025-08-12 18:28:51 +0530

    code reafactord

--- src/main/java/com/example/easymytrip/HelloController.java
+++ src/main/java/com/example/easymytrip/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World pratik thisis the second days ";
+        return "Hello World pratik thisis the second days";
     }
 }
